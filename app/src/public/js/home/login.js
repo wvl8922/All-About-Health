c0df7eb5e88495acdc2e98407708cea88a5b01b2
wvl8922 <wvl8922@gmail.com>
2021-10-09 09:00:01 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,17 @@
 'use strict'
 
-console.log("hello");
+// DOM -> Docum\ent Object Model
 
-console.log("bye");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginbutton = document.querySelector("button");
+
+loginbutton.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        password : password.value,
+        };
+    console.log(req);
+};
