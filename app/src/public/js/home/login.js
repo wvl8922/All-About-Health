07587e417966c958f3b0dbdaba0a29055a87d793
wvl8922 <wvl8922@gmail.com>
2021-10-09 10:08:54 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,6 +23,15 @@ function login(){
         body: JSON.stringify(req),
     })
     .then((res)=>res.json())
-    // .then((res)=>console.log(res));
-    .then(console.log)
+    .then((res)=>{
+        if(res.success){
+            location.href="/";
+        } else{
+            alert(res.msg)
+        }
+    })
+    .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생"));
+    });
+
 };
